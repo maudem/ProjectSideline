33547c91411cde038b23846f02438e85ededf805
Maude <mualth@gmail.com>
2017-08-02 19:03:05 -0400

    landing with working js

--- app/app.js
+++ app/app.js
@@ -1,7 +1,43 @@
+//  var React = require('react');
+//  var router = require('react-router');
+
+// // var ReactDOM = require('react-dom');
+// // //component for individual routes
+
+// //component for all of the routes
+// var Route = router.Router;
+// var hashHistory = router.hashHistory;
+// //catch all route
+// var IndexRoute = router.IndexRoute;
+
+// // import { BrowserRouter, Route } from "react-router-dom"
+// var Main = require('./components/Main');
+// var LandingPage = require("./components/LandingPage");
+// // var CoachesPage = require("./components/CoachesPage");
+// // var CoachAdmin = require("./components/CoachAdmin");
+
+
+// module.exports = (
+//     <Router history={hashHistory}>
+//         <Route path="/" component={Main}>
+//             <Route path="LandingPage" component={LandingPage} />
+//             <IndexRoute component={LandingPage} />
+//         </Route>
+//     </Router>
+// );
+
+// // ReactDOM.render(
+// //     (
+// //         <BrowserRouter history={hashHistory}>
+// //             <Route path="/" component={Main}>
+// //                 <IndexRoute component={LandingPage} />
+// //             </Route>
+// //         </BrowserRouter>
+// //     ), document.getElementById('app'));
+
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var Main = require('./components/Main');
+var routes = require('./config/routes');
 
-ReactDOM.render(<Main />, document.getElementById('app')
-);
+ReactDOM.render(routes, document.getElementById('app'));
